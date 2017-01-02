b87c3ab8ebc2ae2768b0f42b6865fd3265f52ad9
seamanhd <seamanhd@gmail.com>
2017-01-02 10:36:09 -0500

    wrote AJAX script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,94 @@
 // GLOBAL VARIABLES
 //-----------------------------------------------------------------------------
 
-
+var topics = ["giraffe", "lion", "dog", "cat", "horse", "bear"]
 
 
 //-----------------------------------------------------------------------------
 //FUNCTIONS
 //-----------------------------------------------------------------------------
+
+var makeButtons = function() {
+
+	for (var i=0;i<topics.length;i++) {
+		var currentAnimal = topics[i]
+		$("#buttons").append("<button class='getGif' data-animal=" + topics[i] + ">" + topics[i] + "</button>");
+	
+	}
+
+
+};
+
+makeButtons();
+
+
+
+$(".getGif").on("click", function() {
+
+	var searchTerm = $(this).data("animal");
+
+	console.log(searchTerm);
+	var queryURL =  "http://api.giphy.com/v1/gifs/search?q=" +
+        searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+    
+    .done(function(response) {
+    	var results = response.data;
+    	console.log(results);
+
+    	for (var i = 0; i < results.length; i++) {
+    		var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var animalImage = $("<img class='gif'>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              animalImage.attr("src", results[i].images.original_still.url);
+              animalImage.attr("data-still", results[i].images.original_still.url);
+              animalImage.attr("data-animate", results[i].images.fixed_height.url);
+              animalImage.attr("data-state", "still");
+
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(animalImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#display").prepend(gifDiv);
+    	}
+    });
+
+    
+
+    //fixed_height.url
+
+
+
+	});
+
+$(".gif").on("click", function() {
+    	console.log("event");
+    		var state = $(this).attr("data-state");
+
+      if (state === "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    }
+
+    });
